f8e67a91a8ce7ba5151774f3b05a8e3e8666ca83
randomtrydude <randomtrydude@protonmail.com>
2024-12-16 14:38:56 +0100

    Cleared lot of comments Dynamic

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "parse.h"
+#include "extract.h"
 
 #define FAIL_CODE 1
 #define SUCCESS_CODE 0
@@ -10,16 +11,16 @@
 #pragma pack(push, 1) // Disable padding
 // for more info on pragma look into this https://stackoverflow.com/questions/3318410/pragma-pack-effect
 
-typedef struct      // this structure is found in BITMAPFILEHEADER structure in wingdi.h check msndocs for this 
+typedef struct      // this structure is found in BITMAPFILEHEADER structure in wingdi.h check msndocs for this
 {
     uint16_t fileType;        // File type must be 0x4d42 or "BM" in ascii. uint8_t means unsigned integer of size 16 bits or 1 WORD
-    uint32_t fileSize;        // Size of the bitmap file in bytes. DWORD is equal to 32 bits 
-    uint16_t fileReserved1;   // Reserved value, must be 0  
+    uint32_t fileSize;        // Size of the bitmap file in bytes. DWORD is equal to 32 bits
+    uint16_t fileReserved1;   // Reserved value, must be 0
     uint16_t fileReserved2;   // Reserved value, must be 0
-    uint32_t fileOffsetBits;  // Offset value in bytes. From the beginning of the BMPHEAD structure to the bitmap bits.   
+    uint32_t fileOffsetBits;  // Offset value in bytes. From the beginning of the BMPHEAD structure to the bitmap bits.
 } BMPHEAD;
 
-typedef struct 
+typedef struct
 {
     uint32_t size;
     uint32_t width;
@@ -36,133 +37,124 @@ typedef struct
 
 #pragma pack(pop) // Re-enable padding
 
-int ChangeImageBits(uint8_t *initial_pixel_data, int width, int height)       // since we'll be dealing with characters, which are of size 8 bits, we will set pixel data value to uint8_t
+int ChangeImageBits(uint8_t *initial_pixel_data, int width, int height, FILE *fp)
 {
-    // since we are manipulating 24-bit bitmap files, we need 3 bytes. (8 * 3 = 24)
-    int bytes_needed = 3;
+	printf(
+		"\n"
+		"|=====================|\n"
+		"|      EMBEDDING      |\n"
+		"|=====================|\n"
+		);
 
-    // we need this to make sure that the padding portion is not affected
-    // the image repeates a portion onto the output image if padding is also manipulated. 
-    // besides_padding ensures the alignment between total no. of bytes in a row of pixel data and multiple of 4
-    // refer to this for more info: https://www.cs.umd.edu/~meesh/cmsc411/website/projects/outer/memory/align.htm
 
-    int besides_padding = ((bytes_needed * width) + 3 ) & ~3; 
-    printf("\nTotal bytes needed= %d\n",besides_padding);
-    //char* parse_return = parseText();
+    int bytes_needed = 3;  // Each pixel is 3 bytes (RGB)
+    int padding = (4 - (width * bytes_needed) % 4) % 4;  // Row padding to make the row size a multiple of 4
+    int row_size = (width * bytes_needed) + padding;
 
-    // todo: allocate memory dynamically
-    // memory allocation
     char input_text[100];
     int count_input_char_position = 0;
 
-    printf("\nEnter a value to be embedded: \n");
-    scanf("%s", input_text);
-    printf("\nHola %s\n", input_text);
+    printf("Enter a value to be embedded: ");
+    fgets(input_text, sizeof(input_text) , stdin );
+    // Remove newline character if present
+    input_text[strcspn(input_text, "\n")] = '\0';
 
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x+=3)
-        {
-            // pixel holds the combined 24-bit value of an individual BMP image pixel 
-            // for each iteraction, we will take three image pixels to store a single character
-            
-            /*
-                Since a single character takes 8 bytes of space, we will require 3 pixels to store a single character if we choose to only make use of 1 LSB
-                3 pixels = 9 RGB values, 8 RGB values used for hiding the information and the trailing value to indicate continuation of data hiding
-                1 = Continue encoding
-                0 = Finish encoding 
-            */
+    size_t input_text_len = strlen(input_text);
 
-            uint8_t *pixel1 = &initial_pixel_data[x * bytes_needed + y * besides_padding];
-            uint8_t *pixel2 = &initial_pixel_data[(x+1) * bytes_needed + y * besides_padding];
-            uint8_t *pixel3 = &initial_pixel_data[(x+2) * bytes_needed + y * besides_padding];
+    printf("Embedding data: %s\n", input_text);
 
-        
-            // Here, pixel[0], [1] and [2] set the B G R values respectively.
-            /*
-                Understanding negation operation:
-                    The negation operator `~` flips bits. So here, 1 (0000 0001) gets flipped.
-                    So, ~1 is 1111 1110. When you perform the & operation, it ends up setting all 7 bits to 1 and the last bit to 0.
-                    This would pretty much mean every bit except for the least significant one stays the same.
+    // Iterate through the image's pixel data
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x += 3) {  // Process each 3-pixel block
+            if (count_input_char_position < input_text_len) {
+                // For each character, parse it into binary
+                char binary_string[9] = {0}; // since our array will always be 9
+                
+                parseBinary(input_text[count_input_char_position], binary_string);
 
-                What we need in order to embed the data:
-                value = ( value & ∼1 ) | ( bit & 1 )
+                // Embed the binary data into the least significant bits of the RGB values
+                uint8_t *pixel1 = &initial_pixel_data[(x + 0) * bytes_needed + y * row_size];
+                uint8_t *pixel2 = &initial_pixel_data[(x + 1) * bytes_needed + y * row_size];
+                uint8_t *pixel3 = &initial_pixel_data[(x + 2) * bytes_needed + y * row_size];
 
+                // Embed each bit of the character into the RGB channels
+                int j = 0;
+                pixel1[0] = (pixel1[0] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel1[1] = (pixel1[1] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel1[2] = (pixel1[2] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
 
-            */
-            int a[3] = {pixel1[0], pixel1[1], pixel1[2]};
+                pixel2[0] = (pixel2[0] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel2[1] = (pixel2[1] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel2[2] = (pixel2[2] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
 
-            // we have to allocate 9 bytes of memory, 1 for each char 
-            // Since 1 char has 8 bits of info and we have to store each bit as a character
-            // 1 * 8 + 1 = 9 bytes of memory allocated (1 for termination bit)
-            char* abc = (char*) malloc(9 * sizeof(char));
-    
-            // parse the text and store the last binary string in the buffer
-            parseText(input_text[count_input_char_position], &abc);
-            printf("\ninput char: %c\n", input_text[count_input_char_position]);
-            count_input_char_position++;
-            
+                pixel3[0] = (pixel3[0] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel3[1] = (pixel3[1] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
+                pixel3[2] = (pixel3[2] & ~1) | (convertToRawBinary(binary_string[j++]) & 1);
 
-            // this is for debugging 
-            printf("\nlast buffer val: %s\n", abc);
-            int j = 0;
-            printf("\nCheck here\n");
+                // Set the continuation bit if there are more characters to embed
+                pixel3[2] = (pixel3[2] & ~1) | (input_text[count_input_char_position + 1] != '\0');
 
-            pixel1[0] = (pixel1[0] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            pixel1[1] = (pixel1[1] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            pixel1[2] = (pixel1[2] & ~1) | (convertToRawBinary(abc[j++]) & 1);
+                count_input_char_position++;
+            }
 
-            // pixel2[0] = 0;
-            // pixel2[1] = 0;
-            // pixel2[2] = 0;
-            pixel2[0] = (pixel2[0] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            pixel2[1] = (pixel2[1] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            pixel2[2] = (pixel2[2] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-        
-            pixel3[0] = (pixel3[0] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            pixel3[1] = (pixel3[1] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-            // for this, you have to create some form of condition. Treat this as a continuation bit
-
-            pixel3[2] = (pixel3[2] & ~1) | (convertToRawBinary(abc[j++]) & 1);
-
-            printf("\nCheck here\n"); 
-
-            if (input_text[count_input_char_position] == '\0')
+            // Handle end of input: we mark the last pixel with continuation = 0
+            if (input_text[count_input_char_position] == '\0') {
                 return END_OF_INPUT;
-            free(abc);
+            }
         }
     }
-    return SUCCESS_CODE;   // return just in case 
+
+    return SUCCESS_CODE;
 }
 
-int main()
-{
-    FILE *fp;
-    char input_image_name[50] = "input_images/hotel.bmp";
-    fp = fopen(input_image_name,"rb");
 
+int main(int argc , char *argv[])
+{
+
+    if(argc != 3){
+            printf("Usage ./program -embed/extract picture\n"
+                   "Example: ./program -embed picture.bmp\n"
+                   "Example2: ./program -extract picture.bmp\n");
+            exit(FAIL_CODE);
+    }
+    
+    int embed_flag = 0; // initiate at 0 
+    FILE *fp = fopen(argv[2] ,"rb"); // that way we can use any image // but maybe check if in that case?
+    printf("%s" , argv[2]);
     if (!fp)
     {
         printf("No pic :(\n");
-        return FAIL_CODE;       
+        return FAIL_CODE;
     }
     
+      if (strcasecmp(argv[1], "-embed") == 0 ) // Embedding data
+    {
+        embed_flag = 1; // only change if we embed
+    }
+      else if(strcasecmp(argv[1] , "-extract") != 0){
+      		printf("Error parsing options");
+      		exit(FAIL_CODE); // we could use some different code base on the return 
+      	}
+    
+
+
     BMPHEAD bitmapFile;
     BMPINFO bitmapInfo;
 
-    // reading file headers
+    // Reading file headers
     fread(&bitmapFile, sizeof(BMPHEAD), 1, fp);
     fread(&bitmapInfo, sizeof(BMPINFO), 1, fp);
-    
+
     int width = bitmapInfo.width;
     int height = bitmapInfo.height;
 
     printf("Width: %d\n", width);
     printf("Height: %d\n", height);
 
-    // read image data
+    // Read image data
     uint8_t *image_data = (uint8_t *) malloc(bitmapInfo.imgSize);
 
+    printf("\nBitmap offset: %x\n", bitmapFile.fileOffsetBits);
     if (image_data == NULL)
     {
         printf("Error: Memory allocation failed.\n");
@@ -170,24 +162,39 @@ int main()
         return FAIL_CODE;
     }
 
-    fseek(fp, bitmapFile.fileOffsetBits, SEEK_SET);     // SEEK_SET is a macro defined as 0 aka explicitly setting an offset https://stackoverflow.com/questions/56433377/why-is-the-beginning-of-a-c-file-stream-called-seek-set
+    // Move to the offset of the pixel data
+    fseek(fp, bitmapFile.fileOffsetBits, SEEK_SET);
+
+    // Read the pixel data
     fread(image_data, bitmapInfo.imgSize, 1, fp);       // image_data stores the pixel values of the bitmap image
+
     fclose(fp);
 
-    // perform image manipulation
-    printf("\nEmbed or extract? (e/x)\n");
-    if (getc(stdin) == 'e')
-        ChangeImageBits(image_data, width, height);
+    // Now perform image manipulation or extraction based on the user's choice
+    if (embed_flag == 1)
+    {
+        // Embedding case, modify the image and save it
+        const char output[] = "output.bmp"; // regardless of option , this shall be the out picture
+        fp = fopen(output, "wb");            
+        
+        fwrite(&bitmapFile, sizeof(BMPHEAD), 1, fp);
+        fwrite(&bitmapInfo, sizeof(BMPINFO), 1, fp);
+
+        // Call embedding function to modify pixel data
+        ChangeImageBits(image_data, width, height, fp);
+
+        // Write modified image data back to file
+        fwrite(image_data, bitmapInfo.imgSize, 1, fp);
+        fclose(fp);
+    }
     else
+    {
+        // Extraction case, read from the modified image
+        ExtractImageBits(image_data, width, height); // Process the modified image data
+    }
 
-    // write modified image data to a new file
-    fp = fopen("output_images/modified_hill.bmp", "wb");            // opens a write file for binary
-    fwrite(&bitmapFile, sizeof(BMPHEAD), 1, fp);
-    fwrite(&bitmapInfo, sizeof(BMPINFO), 1, fp);
-    fwrite(image_data, bitmapInfo.imgSize, 1, fp);
-    fclose(fp);
-
-
+    // Free allocated memory
     free(image_data);
     return SUCCESS_CODE;
 }
+
